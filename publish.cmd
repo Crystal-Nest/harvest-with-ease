66a0e75e986a2a656ec36bc601c7b02469c9e0d0
Crystal Spider <riccardo.chimisso@gmail.com>
2023-03-03 22:13:33 +0100

    [Forge/Fabric]: Updated publish.cmd for Publish on Modrinth #17

--- publish.cmd
+++ publish.cmd
@@ -1,6 +1,8 @@
 cd ./fabric
 powershell -Command "./gradlew githubRelease";
 powershell -Command "./gradlew curseforge";
+powershell -Command "./gradlew modrinth";
 cd ../forge
 powershell -Command "./gradlew githubRelease";
 powershell -Command "./gradlew curseforge";
+powershell -Command "./gradlew modrinth";

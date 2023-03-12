c4fa7d65c28b5a61d214e30660e435d2f8477a7a
Crystal Spider <riccardo.chimisso@gmail.com>
2023-03-12 12:48:08 +0100

    [Forge]: Extracted logic to check if the player can interact.

--- forge/src/main/java/crystalspider/harvestwithease/handlers/RightClickBlockHandler.java
+++ forge/src/main/java/crystalspider/harvestwithease/handlers/RightClickBlockHandler.java
@@ -57,7 +57,7 @@ public class RightClickBlockHandler {
   public static void handle(RightClickBlock event) {
     Level world = event.getWorld();
     Player player = event.getPlayer();
-    if (!player.isSpectator() && event.getUseBlock() != Result.DENY && event.getUseItem() != Result.DENY && event.getResult() != Result.DENY) {
+    if (canInteract(player, event)) {
       BlockPos blockPos = event.getPos();
       BlockState blockState = world.getBlockState(blockPos);
       InteractionHand interactionHand = getInteractionHand(player);
@@ -81,6 +81,17 @@ public class RightClickBlockHandler {
         }
       }
     }
+  }
+
+  /**
+   * Checks whether the {@link Player} can interact with the {@link RightClickBlock event}.
+   * 
+   * @param player
+   * @param event
+   * @return whether the {@link Player} can interact with the {@link RightClickBlock event}.
+   */
+  private static boolean canInteract(Player player, RightClickBlock event) {
+    return !player.isSpectator() && event.getUseBlock() != Result.DENY && !(HarvestWithEaseConfig.getRequireHoe() || event.getUseItem() == Result.DENY) && event.getResult() != Result.DENY;
   }
 
   /**

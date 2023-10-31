321f213758fe86d40cec3d2047be0d18d70dc6bc
Crystal Spider <riccardo.chimisso@gmail.com>
2023-10-31 13:16:57 +0100

    [FF]: Make breaking crops give XP too if config is enabled. #27

--- fabric/src/main/java/crystalspider/harvestwithease/HarvestWithEaseLoader.java
+++ fabric/src/main/java/crystalspider/harvestwithease/HarvestWithEaseLoader.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import crystalspider.harvestwithease.config.HarvestWithEaseConfig;
-import crystalspider.harvestwithease.handlers.UseBlockHandler;
+import crystalspider.harvestwithease.handler.PlayerBlockBreakHandler;
+import crystalspider.harvestwithease.handler.UseBlockHandler;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraftforge.api.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig.Type;
@@ -28,5 +30,6 @@ public class HarvestWithEaseLoader implements ModInitializer {
   public void onInitialize() {
     ModLoadingContext.registerConfig(MODID, Type.COMMON, HarvestWithEaseConfig.SPEC);
     UseBlockCallback.EVENT.register(UseBlockHandler::handle);
+    PlayerBlockBreakEvents.AFTER.register(PlayerBlockBreakHandler::handle);
   }
 }

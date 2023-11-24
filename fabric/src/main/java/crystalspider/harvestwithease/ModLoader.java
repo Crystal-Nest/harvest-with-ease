4b54fe302129a4d29335143e7705a3a259a34c07
Crystal Spider <riccardo.chimisso@gmail.com>
2023-11-24 18:11:34 +0100

    [FF]: Crops do not drop experience when harvested via right-click #28 and Harvest multiple crops at once #30

--- fabric/src/main/java/crystalspider/harvestwithease/ModLoader.java
+++ fabric/src/main/java/crystalspider/harvestwithease/ModLoader.java
@@ -1,34 +1,26 @@
 package crystalspider.harvestwithease;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import crystalspider.harvestwithease.config.HarvestWithEaseConfig;
+import crystalspider.harvestwithease.config.ModConfig;
 import crystalspider.harvestwithease.handler.PlayerBlockBreakHandler;
 import crystalspider.harvestwithease.handler.UseBlockHandler;
 import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.config.ModConfig.Type;
 
 /**
  * Harvest with ease mod loader.
  */
-public class HarvestWithEaseLoader implements ModInitializer {
+public class ModLoader implements ModInitializer {
   /**
    * Harvest with ease mod ID.
    */
-  public static final String MODID = "harvestwithease";
-
-  /**
-   * Logger.
-   */
-  public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+  public static final String MOD_ID = "harvestwithease";
 
   @Override
   public void onInitialize() {
-    ForgeConfigRegistry.INSTANCE.register(MODID, ModConfig.Type.COMMON, HarvestWithEaseConfig.SPEC);
+    ForgeConfigRegistry.INSTANCE.register(MOD_ID, Type.COMMON, ModConfig.SPEC);
     UseBlockCallback.EVENT.register(UseBlockHandler::handle);
     PlayerBlockBreakEvents.AFTER.register(PlayerBlockBreakHandler::handle);
   }

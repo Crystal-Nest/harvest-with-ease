2d5bcfd1856b11cc831dd7659c76a350ca3e0446
Crystal Spider <riccardo.chimisso@gmail.com>
2023-11-24 17:24:23 +0100

    [FF]: Crops do not drop experience when harvested via right-click #28 and Harvest multiple crops at once #30

--- fabric/src/main/java/crystalspider/harvestwithease/api/HarvestWithEaseAPI.java
+++ fabric/src/main/java/crystalspider/harvestwithease/api/HarvestWithEaseAPI.java
@@ -1,36 +1,41 @@
 package crystalspider.harvestwithease.api;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+import java.util.Optional;
 
-import crystalspider.harvestwithease.config.HarvestWithEaseConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import crystalspider.harvestwithease.ModLoader;
+import crystalspider.harvestwithease.config.ModConfig;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CocoaBlock;
 import net.minecraft.block.CropBlock;
 import net.minecraft.block.NetherWartBlock;
+import net.minecraft.item.ToolItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 
 /**
  * Utility class that serves as an API for mods interfacing with Harvest With Ease mod.
  */
 public final class HarvestWithEaseAPI {
-  private HarvestWithEaseAPI() {}
-
   /**
-   * Checks whether or not the block passed as parameter is a crop that can be harvested using this mod.
-   * 
-   * @param block
-   * @return whether the given block it's a valid crop.
+   * Logger.
    */
-  public static boolean isCrop(Block block) {
-    // Hardcoded block id, might consider importing the mod and get the id from its list of registered blocks.
-    return !getKey(block).equals("farmersdelight:tomatoes") && isBreakableCrop(block);
-  }
+  private static final Logger LOGGER = LoggerFactory.getLogger(ModLoader.MOD_ID);
+
+  private HarvestWithEaseAPI() {}
 
   /**
    * Checks whether the given block is a crop that can be broken and, optionally, drop xp.
@@ -38,8 +43,8 @@ public final class HarvestWithEaseAPI {
    * @param block
    * @return whether the given block is a valid breakable crop.
    */
-  public static boolean isBreakableCrop(Block block) {
-    return block instanceof CropBlock || block instanceof NetherWartBlock || block instanceof CocoaBlock || HarvestWithEaseConfig.getCrops().contains(getKey(block));
+  public static boolean isCrop(Block block) {
+    return block instanceof CropBlock || block instanceof NetherWartBlock || block instanceof CocoaBlock || ModConfig.getCrops().contains(getKey(block));
   }
 
   /**
@@ -47,9 +52,9 @@ public final class HarvestWithEaseAPI {
    * 
    * @param blockState - {@link BlockState state} to take the age property from.
    * @return the age property from the given blockState.
-   * @throws NullPointerException - if the age property was null.
-   * @throws NoSuchElementException - if no value for the age property is present.
-   * @throws ClassCastException - if the age property is not an {@link IntProperty}.
+   * @throws NullPointerException if the age property was null.
+   * @throws NoSuchElementException if no value for the age property is present.
+   * @throws ClassCastException if the age property is not an {@link IntProperty}.
    */
   public static IntProperty getAge(BlockState blockState) throws NullPointerException, NoSuchElementException, ClassCastException {
     return (IntProperty) blockState.getProperties().stream().filter(property -> property.getName().equals("age")).findFirst().orElseThrow();
@@ -76,20 +81,94 @@ public final class HarvestWithEaseAPI {
    * @throws ClassCastException if the age property is not an {@link IntProperty}.
    */
   public static boolean isMature(BlockState blockState) throws NullPointerException, NoSuchElementException, ClassCastException {
-    return HarvestWithEaseAPI.isMature(blockState, HarvestWithEaseAPI.getAge(blockState));
+    return isMature(blockState, getAge(blockState));
   }
-
 
   /**
    * Checks whether the given crop is a multi-block crop (a crop made of multiple vertically connected blocks).
    * 
-   * @param level - {@link World} in which the crop is placed.
+   * @param world - {@link World} in which the crop is placed.
    * @param blockState - {@link BlockState} of the crop.
    * @param blockPos - {@link BlockPos} of the crop.
    * @return whether the given crop is a multi-block crop.
    */
-  public static boolean isTallCrop(World level, BlockState blockState, BlockPos blockPos) {
-    return blockState.isIn(BlockTags.CROPS) && level.getBlockState(blockPos.down()).isOf(blockState.getBlock()) || level.getBlockState(blockPos.up()).isOf(blockState.getBlock());
+  public static boolean isTallCrop(World world, BlockState blockState, BlockPos blockPos) {
+    return blockState.isIn(BlockTags.CROPS) && world.getBlockState(blockPos.down()).isOf(blockState.getBlock()) || world.getBlockState(blockPos.up()).isOf(blockState.getBlock());
+  }
+
+  /**
+   * Checks whether the given {@link ToolItem tool} has an high enough tier for multi-harvest.
+   * 
+   * @param tool
+   * @return whether the given {@link ToolItem tool} is allowed to multi-harvest.
+   */
+  public static boolean isTierForMultiHarvest(ToolItem tool) {
+    ToolMaterial toolTier = tool.getMaterial();
+    String configTier = ModConfig.getMultiHarvestStartingTier();
+    return toolTier.getMiningLevel() >= getTierLevel(configTier);
+  }
+
+  /**
+   * Checks if the given tier string reference is in the given list of tiers.
+   * 
+   * @param tiers
+   * @param tierRef
+   * @return whether {@code tierRef} represents a {@link ToolMaterial} in {@code tiers}.
+   */
+  public static boolean isTierIn(List<ToolMaterial> tiers, String tierRef) {
+    return tiers.stream().anyMatch(tier -> matchesTier(tierRef, tier));
+  }
+
+  /**
+   * Checks if the given {@link ToolMaterial} is in the given list of tiers.
+   * 
+   * @param tiers
+   * @param tier
+   * @return whether {@code tier} is a {@link ToolMaterial} in {@code tiers}.
+   */
+  public static boolean isTierIn(List<ToolMaterial> tiers, ToolMaterial tier) {
+    return isTierIn(tiers, tier.toString());
+  }
+
+  /**
+   * Gets the proper tier level for the given {@link ToolMaterial}.
+   * <p>
+   * If the tier is not in the {@link TierSortingRegistry} then the level is {@code -1}.
+   * 
+   * @param tier
+   * @return tier level.
+   */
+  public static int getTierLevel(ToolMaterial tier) {
+    return tier.getMiningLevel();
+  }
+
+  /**
+   * Gets the proper tier level for the given {@link ToolMaterial} reference.
+   * <p>
+   * If {@code tierRef} is {@code "none"} then the level is {@code -1}.
+   * <p>
+   * If the tier is not in the {@link ToolMaterials} then the level is {@code 0} (same as Vanilla wood tier).
+   * 
+   * @param tierRef
+   * @return tier level.
+   */
+  public static int getTierLevel(String tierRef) {
+    try {
+      return tierRef.equalsIgnoreCase("none") ? -1 : getTierLevel(Stream.of(ToolMaterials.values()).filter(tier -> matchesTier(tierRef, tier)).findFirst().orElseThrow());
+    } catch (NoSuchElementException e) {
+      return 0;
+    }
+  }
+
+  /**
+   * Checks whether the given {@link ToolMaterial} matches the given tier reference.
+   * 
+   * @param tier
+   * @param tierRef
+   * @return whether {@code tierRef} represents {@code tier}.
+   */
+  public static boolean matchesTier(String tierRef, ToolMaterial tier) {
+    return tier.toString().equalsIgnoreCase(tierRef);
   }
 
   /**
@@ -99,6 +178,11 @@ public final class HarvestWithEaseAPI {
    * @return in-game ID of the given block.
    */
   private static String getKey(Block block) {
-    return Registry.BLOCK.getKey(block).get().getValue().toString();
+    Optional<RegistryKey<Block>> key = Registry.BLOCK.getKey(block);
+    if (key.isPresent()) {
+      return key.get().getValue().toString();
+    }
+    LOGGER.debug("Couldn't get key for block [" + block + "].");
+    return "";
   }
 }

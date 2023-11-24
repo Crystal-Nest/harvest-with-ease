c7ebe43bac80a0f6cc956ca4427048c7d795f082
Crystal Spider <riccardo.chimisso@gmail.com>
2023-11-24 17:52:48 +0100

    [FF]: Make breaking crops give XP too if config is enabled. #27

--- fabric/src/main/java/crystalspider/harvestwithease/ModLoader.java
+++ fabric/src/main/java/crystalspider/harvestwithease/ModLoader.java
@@ -1,9 +1,6 @@
 package crystalspider.harvestwithease;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import crystalspider.harvestwithease.config.HarvestWithEaseConfig;
+import crystalspider.harvestwithease.config.ModConfig;
 import crystalspider.harvestwithease.handler.PlayerBlockBreakHandler;
 import crystalspider.harvestwithease.handler.UseBlockHandler;
 import net.fabricmc.api.ModInitializer;
@@ -15,20 +12,15 @@ import net.minecraftforge.fml.config.ModConfig.Type;
 /**
  * Harvest with ease mod loader.
  */
-public class HarvestWithEaseLoader implements ModInitializer {
+public class ModLoader implements ModInitializer {
   /**
    * Harvest with ease mod ID.
    */
-  public static final String MODID = "harvestwithease";
-
-  /**
-   * Logger.
-   */
-  public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+  public static final String MOD_ID = "harvestwithease";
 
   @Override
   public void onInitialize() {
-    ModLoadingContext.registerConfig(MODID, Type.COMMON, HarvestWithEaseConfig.SPEC);
+    ModLoadingContext.registerConfig(MOD_ID, Type.COMMON, ModConfig.SPEC);
     UseBlockCallback.EVENT.register(UseBlockHandler::handle);
     PlayerBlockBreakEvents.AFTER.register(PlayerBlockBreakHandler::handle);
   }
---
+++ forge/src/main/java/crystalspider/harvestwithease/ModLoader.java
@@ -0,0 +1,33 @@
+package crystalspider.harvestwithease;
+
+import crystalspider.harvestwithease.config.ModConfig;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig.Type;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.SimpleChannel;
+
+/**
+ * Harvest with ease mod loader.
+ */
+@Mod(ModLoader.MOD_ID)
+public class ModLoader {
+  /**
+   * ID of this mod.
+   */
+  public static final String MOD_ID = "harvestwithease";
+
+  /**
+   * Network channel protocol version.
+   */
+  public static final String PROTOCOL_VERSION = "1.19.2-8.0";
+  /**
+   * {@link SimpleChannel} instance for compatibility client-server.
+   */
+  public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(new ResourceLocation(MOD_ID, "main"), () -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
+
+  public ModLoader() {
+    ModLoadingContext.get().registerConfig(Type.COMMON, ModConfig.SPEC);
+  }
+}

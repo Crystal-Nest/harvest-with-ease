5ef8b56273975c42ca22775ddbbcfe68b9cf4f60
Crystal Spider <riccardo.chimisso@gmail.com>
2024-06-26 21:37:59 +0200

    Backport to 1.20.1

--- common/src/main/java/it/crystalnest/harvest_with_ease/config/ModConfig.java
+++ common/src/main/java/it/crystalnest/harvest_with_ease/config/ModConfig.java
@@ -5,11 +5,7 @@ import it.crystalnest.cobweb.api.item.TierUtils;
 import it.crystalnest.harvest_with_ease.Constants;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.level.block.CropBlock;
-import net.neoforged.neoforge.common.ModConfigSpec;
-import net.neoforged.neoforge.common.ModConfigSpec.BooleanValue;
-import net.neoforged.neoforge.common.ModConfigSpec.ConfigValue;
-import net.neoforged.neoforge.common.ModConfigSpec.EnumValue;
-import net.neoforged.neoforge.common.ModConfigSpec.IntValue;
+import net.minecraftforge.common.ForgeConfigSpec;
 import org.jetbrains.annotations.ApiStatus;
 
 import java.util.Collections;
@@ -29,49 +25,49 @@ public final class ModConfig extends CommonConfig {
   /**
    * List of additional in-game IDs for crops that need to be supported but do not extend {@link CropBlock}.
    */
-  private ConfigValue<List<? extends String>> crops;
+  private ForgeConfigSpec.ConfigValue<List<? extends String>> crops;
 
   /**
    * List in-game IDs for crops that under no condition can be right-click harvested.
    */
-  private ConfigValue<List<? extends String>> blacklist;
+  private ForgeConfigSpec.ConfigValue<List<? extends String>> blacklist;
 
   /**
    * Whether holding a hoe (either hands) is required.
    */
-  private BooleanValue requireHoe;
+  private ForgeConfigSpec.BooleanValue requireHoe;
 
   /**
    * Amount of damage to deal on a hoe when it is used to right-click harvest.
    * Effective only if greater than 0 and {@link #requireHoe} is true.
    */
-  private IntValue damageOnHarvest;
+  private ForgeConfigSpec.IntValue damageOnHarvest;
 
   /**
    * Amount of experience to grant on harvest.
    * Effective only if greater than 0.
    */
-  private IntValue grantedExp;
+  private ForgeConfigSpec.IntValue grantedExp;
 
   /**
    * Tool tier starting from which it is possible to harvest multiple crops at once.
    */
-  private ConfigValue<String> multiHarvestStartingTier;
+  private ForgeConfigSpec.ConfigValue<String> multiHarvestStartingTier;
 
   /**
    * Starting harvest area size (square side length).
    */
-  private EnumValue<AreaSize> areaStartingSize;
+  private ForgeConfigSpec.EnumValue<AreaSize> areaStartingSize;
 
   /**
    * Increment step for the harvest area size with higher tool tiers.
    */
-  private EnumValue<AreaStep> areaIncrementStep;
+  private ForgeConfigSpec.EnumValue<AreaStep> areaIncrementStep;
 
   /**
    * @param builder configuration builder.
    */
-  private ModConfig(ModConfigSpec.Builder builder) {
+  private ModConfig(ForgeConfigSpec.Builder builder) {
     super(builder);
   }
 
@@ -183,7 +179,7 @@ public final class ModConfig extends CommonConfig {
   }
 
   @Override
-  protected void define(ModConfigSpec.Builder builder) {
+  protected void define(ForgeConfigSpec.Builder builder) {
     crops = builder.comment(" List of in-game IDs of additional crops.").defineListAllowEmpty(List.of("crops"), Collections::emptyList, this::stringListValidator);
     blacklist = builder.comment(" List of in-game IDs for crops that under no condition can be right-click harvested.").defineListAllowEmpty(List.of("blacklist"), Collections::emptyList, this::stringListValidator);
     requireHoe = builder.comment(" Require holding a hoe (either hands) to right-click harvest.").define("require hoe", false);

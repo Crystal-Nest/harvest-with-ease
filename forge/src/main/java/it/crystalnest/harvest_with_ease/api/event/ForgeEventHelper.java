5d81ed0b938d287d260c0f7e3d7cae82e4565c05
Crystal Spider <riccardo.chimisso@gmail.com>
2024-06-27 15:38:30 +0200

    Add ApiStatus.Internal annotation to event helpers.

--- forge/src/main/java/it/crystalnest/harvest_with_ease/api/event/ForgeEventHelper.java
+++ forge/src/main/java/it/crystalnest/harvest_with_ease/api/event/ForgeEventHelper.java
@@ -11,11 +11,13 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.common.MinecraftForge;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * Forge event helper.
  */
+@ApiStatus.Internal
 public class ForgeEventHelper implements EventHelper {
   @Override
   public boolean fireHarvestCheckEvent(Level level, BlockState crop, BlockPos pos, Direction face, @Nullable BlockHitResult hitResult, Player player, InteractionHand hand) {

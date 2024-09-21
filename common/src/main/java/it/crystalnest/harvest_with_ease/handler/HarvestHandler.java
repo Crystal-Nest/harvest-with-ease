e336dd1b0cfc38a3c6fe624611c1c87ec84bf66d
Crystal Spider <riccardo.chimisso@gmail.com>
2024-09-21 13:07:06 +0200

    Fix [#50]

--- common/src/main/java/it/crystalnest/harvest_with_ease/handler/HarvestHandler.java
+++ common/src/main/java/it/crystalnest/harvest_with_ease/handler/HarvestHandler.java
@@ -107,6 +107,7 @@ public abstract class HarvestHandler {
    * @param age crop age.
    * @param crop crop.
    * @param pos crop position.
+   * @param originalPos clicked position.
    * @param face clicked face.
    * @param hitResult {@link BlockHitResult}.
    * @param player player.
@@ -203,6 +204,7 @@ public abstract class HarvestHandler {
    * @param level level.
    * @param crop crop.
    * @param pos crop position.
+   * @param originalPos clicked position.
    * @param face clicked face.
    * @param hitResult {@link BlockHitResult}.
    * @param player player.
